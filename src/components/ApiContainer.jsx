a8d69aab4aa8da9a6f919ed6e936ef2b4819a5e6
Rex Osariemen <rexosariemen@gmail.com>
2019-12-03 12:13:51 -0500

    add components and routers

---
+++ src/components/ApiContainer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ApiContainer extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <h1>
+          Api Page
+        </h1>
+      </div>
+    );
+  }
+}
+
+
+export default ApiContainer;
---
+++ src/components/SignupContainer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class SignupContainer extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <h1>
+          Signup Page
+        </h1>
+      </div>
+    );
+  }
+}
+
+
+export default SignupContainer;
